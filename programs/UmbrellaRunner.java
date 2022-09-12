a23ed1bea837012254820e264218027e0dde9833
unknown <sinchanashindhe11@gmail.com>
2022-09-12 08:15:44 +0530

    my first push

---
+++ programs/UmbrellaRunner.java
@@ -0,0 +1,12 @@
+class UmbrellaRunner{
+	public static void main(String[] game){
+		Umbrella.Open();
+		Umbrella.Protect();
+		Umbrella.Fold();
+		Umbrella.PressButton();
+		Umbrella.ProvideShade();
+		Umbrella.SlideUp();
+		Umbrella.SlideDown();
+	}
+	
+}
