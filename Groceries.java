9259482d994aefae5d1bb65eb4c09ae60cf138be
unknown <sinchanashindhe11@gmail.com>
2022-09-13 10:42:41 +0530

    food

---
+++ Groceries.java
@@ -0,0 +1,125 @@
+class Groceries{
+	
+	public static void main(String[] args){
+	
+		int Sugar=40;		
+		int Salt=22;
+		int Jaggery=45;
+		int Dhal=48;
+		int Rice=40;
+		int Wheat=45;
+		int Poha=40;
+		int Maggie=20;
+		int Soap=30;
+		int Bakingsoda=10;
+		int Vinegar =40;
+		int Cookingoil=130;
+		int Kesari=10;
+		int Gingerpaste=20;
+		int Rava=30;
+		int Cornflour=30;
+		int Peas=60;
+		int cherry=90;
+		int Spinach=23;
+		int Tomatato=30;
+		int Coconut=25;
+		int Maize=150;
+		int Shampoo=180;
+		int Washingpowder=20;
+		int Groundnut=40;
+		int Cloves=45;
+		int Turmeric =20;
+		int Caromseeds=38;
+		int pepper=35;
+		int Fingermillets=88;
+		int Almond=250;
+		int Dates=70;
+		int Blackrice=207;
+	
+		
+		
+		
+		int[] items={Sugar,Salt,Jaggery,Dhal,Rice,Wheat,Poha,Maggie,Soap,Bakingsoda,Vinegar, Cookingoil,Kesari,
+        Gingerpaste,Rava,Cornflour,Peas,cherry,Spinach,Tomatato,Coconut,Maize,Shampoo,Washingpowder,Groundnut,Cloves,Turmeric,
+		Caromseeds,pepper,Fingermillets,Almond,Dates,Blackrice};
+		
+		int Total=items.length;
+		
+		 System.out.println("Total items="+Total);
+		 
+		 
+		
+		int ref0= items[0];
+		int ref1= items[1];
+		int ref2= items[2];
+		int ref3= items[3];
+		int ref4= items[4];
+		int ref5= items[5];
+		int ref6= items[6];
+		int ref7= items[7];
+		int ref8= items[8];
+		int ref9= items[9];
+		int ref10= items[10];
+		int ref11= items[11];
+		int ref12= items[12];
+		int ref13= items[13];
+		int ref14= items[14];
+		int ref15= items[15];
+		int ref16= items[16];
+		int ref17= items[17];
+		int ref18= items[18];
+		int ref19= items[19];
+		int ref20= items[20];
+		int ref21= items[21];
+		int ref22= items[22];
+		int ref23= items[23];
+		int ref24= items[24];
+		int ref25= items[25];
+		int ref26= items[26];
+		int ref27= items[27];
+		int ref28= items[28];
+		int ref29= items[29];
+		int ref30= items[30];
+		int ref31= items[31];
+		int ref32= items[32];
+		
+		System.out.println("groceries item 1="+ref0);
+		System.out.println("groceries item 2="+ref1);
+		System.out.println("groceries item 3="+ref2);
+		System.out.println("groceries item 4="+ref3);
+		System.out.println("groceries item 5="+ref4);
+		System.out.println("groceries item 6="+ref5);
+		System.out.println("groceries item 7="+ref6);
+		System.out.println("groceries item 8="+ref7);
+		System.out.println("groceries item 9="+ref8);
+		System.out.println("groceries item 10="+ref9);
+		System.out.println("groceries item 11="+ref10);
+		System.out.println("groceries item 12="+ref11);
+		System.out.println("groceries item 13="+ref12);
+		System.out.println("groceries item 14="+ref13);
+		System.out.println("groceries item 15="+ref14);
+		System.out.println("groceries item 16="+ref15);
+		System.out.println("groceries item 17="+ref16);
+		System.out.println("groceries item 18="+ref17);
+		System.out.println("groceries item 19="+ref18);
+		System.out.println("groceries item 20="+ref19);
+		System.out.println("groceries item 21="+ref20);
+		System.out.println("groceries item 22="+ref21);
+		System.out.println("groceries item 23="+ref22);
+		System.out.println("groceries item 24="+ref23);
+		System.out.println("groceries item 25="+ref24);
+		System.out.println("groceries item 26="+ref25);
+		System.out.println("groceries item 27="+ref26);
+		System.out.println("groceries item 28="+ref27);
+		System.out.println("groceries item 29="+ref28);
+		System.out.println("groceries item 30="+ref29);
+		System.out.println("groceries item 31="+ref30);
+		System.out.println("groceries item 32="+ref31);
+		System.out.println("groceries item 33="+ref32);
+	}
+}
+		
+		
+			 
+		 
+	
