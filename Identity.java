5d3de804c60fd26bc99622ac6d8486263441e930
unknown <sinchanashindhe11@gmail.com>
2022-09-15 08:07:53 +0530

    sinchana

---
+++ Identity.java
@@ -0,0 +1,50 @@
+class Identity{
+	static void printName(String name)
+	{
+		System.out.println("printName");
+		System.out.println("Name entered:"+name);
+	}
+	static void printNameAndEmail(String name,String Email)
+	{
+		System.out.println("print name and email");
+		System.out.println("Name and email entered:"+name+Email);
+	}
+	static void printNameAndMobileNO(String name,int MobileNo)
+	{
+		System.out.println("print name and mobileno");
+		System.out.println("Name and mobile no entered :"+name+MobileNo);
+	}
+    static void printNameAndgender(String name,String gender)
+	{
+		
+		System.out.println("print name and gender");
+		System.out.println("Name and gender entered :"+name+gender);
+	}
+	static void printNameAndworking(String name,boolean working)
+	{
+		System.out.println("print name and working");
+		System.out.println("Name and working entered :"+name+working);
+	}
+	static void printNameAndshirtSize(String name,int shirtSize)
+	{
+		System.out.println("print name and shirtSize");
+		System.out.println("Name and shirtSize entered :"+name+shirtSize);
+	}
+	static void printNameAndpantSize(String name,double pantSize)
+	{
+		System.out.println("print name and pant size");
+		System.out.println("Name and pantSize entered :"+name+pantSize);
+	}
+		static void printNameweightheight(String name,float weight,float height)
+	{
+		System.out.println("print name weight and height");
+		System.out.println("Name weight and height entered :"+name+weight+height);
+
+
+		
+		
+		
+		
+	}
+	
+}
---
+++ MobileNumber.java
@@ -0,0 +1,55 @@
+class MobileNumber{
+	
+	static String getNameByMobile(long number)
+	{
+		
+		System.out.println("Entered Mobile Number");
+		if(number==6363354140L){
+			System.out.println("It is Sinchana Number");
+			return "sinchana";
+		}
+			
+			if(number==9741426747L){
+				System.out.println("It is Reshma Number");
+				return "Reshma";
+			}
+			if(number==9741426748L){
+				System.out.println("It is Sachin Number");
+				return "Sachin";
+			}
+			if(number==9741426749L){
+				System.out.println("It is Manjula Number");
+				return"manjula";
+			}
+			if(number==9741426749L){
+				System.out.println("It is Manoj Number");
+				return"manu";
+			}
+			if(number==9741423467L){
+				System.out.println("It is Kaveri Number");
+				return"Kaveri";
+			}
+			if(number==9741426749L){
+				System.out.println("It is Ananaya Number");
+				return"Ananaya";
+			}
+			if(number==9741426749L){
+				System.out.println("It is Varsha Number");
+				return"Varsha";
+			}
+			if(number==6356426749L){
+				System.out.println("It is Madhuri Number");
+				return"Madhuri";
+			}
+			if(number==9871426749L){
+				System.out.println("It is Kushal Number");
+				return"Kushal";
+			}
+			if(number==9741426749L){
+				System.out.println("It is Bavya Number");
+				return"Bavya";
+			}
+				
+		 return null;	
+	}
+}
