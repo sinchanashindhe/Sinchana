40dc4a9c6cf24412339305063a8e3fc265bdd73c
sinchanashindhe <113296319+sinchanashindhe@users.noreply.github.com>
2022-09-28 11:42:59 +0530

    Add files via upload

---
+++ MovieDataOperatorRunner.java
@@ -0,0 +1,24 @@
+class MovieDataOperatorRunner{
+	
+	public static void main(String[] args)
+	
+	{
+	System.out.println("Bettadha Hoovu");
+    MovieDataOperator.save("Kotigobba-3");
+    MovieDataOperator.save("om");	
+	 MovieDataOperator.save("Akasmikha");	
+    MovieDataOperator.save("Rajkumara");	
+    MovieDataOperator.save("Guru Shishya");	 
+	 MovieDataOperator.display();
+	 System.out.println("Exit");
+	 
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
