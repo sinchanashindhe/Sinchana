0eead212e48e9964c7133c73eede3517dfa6ded1
sinchanashindhe <113296319+sinchanashindhe@users.noreply.github.com>
2022-09-30 09:49:58 +0530

    Add files via upload

---
+++ Sinchanajavatest.java
@@ -0,0 +1,105 @@
+    qiuestion 1 platform independent doesnot depends upon any platform it can be run in different platform 
+eg: bytecode is the platform independent variable it can converts the .java to class java.
+
+
+    question 2         primitive:    * primitive datatype can hold the simple datatypes and they dont have 
+			                the members in the primitive datatypes  
+                            * primitive datatypes consists of 8 datatypes
+                            * primitive data types doesnot contain thestates and behaviour
+							eg:byte
+							   int
+							   short
+							   float
+							   long
+							   double
+							   boolean
+							   char
+							
+							
+							
+			non primitive:  *nonprimitive datatypes having states and behaviour
+                             *non primitive data types consists of n number of datatypes
+							 eg: Array,string
+							 
+							 
+	
+	question 3 class is collection of objects and these objective contains states and behaviour
+	            any program which contains states and behaviour
+				it is the syntax which can be started as classname
+				eg:
+				class javatest{
+					
+				}
+				here javatest is the class name
+	
+	
+	           members it is the object living or non living things
+			   objects means  whatever we visible in real world
+			   object having 2 types namely
+			   *states
+			   *behaviour
+			   eg:
+			   
+	question 6 main method is the metod in which the heap memory ther is a stack ,meatspace,constant pool
+             and instant pool,class loader
+           stack memeory is the method in which they execute the program
+           	class loader in this the class name are stored	
+			constant pool in this memeory we store the numerical values
+			instant pool is the memory in which the they stored the 
+            			
+			   
+question 4 class Marks{
+	       static int marks;
+	       static int marks;
+	
+	
+	      class MarksRunner{
+		public satic void main(String[] args)
+		marks .marks 1=20;
+		
+		
+		
+		
+	}
+
+}		   
+	question 8 class Time{
+		public static void main(String[] args)
+		{
+			float hours=24f;
+			float  seconds=3600f;
+			System.out.println("Hours into seconds")
+			
+		}
+		
+		
+	}	   
+		
+question 7 class Reverse{
+	public static void main(String[] args)
+	{
+		int reverse=1,2,3;
+		System.out.println("print the reverse number")
+		
+	}
+}		
+			   
+			   
+			   
+			   
+			   
+			   
+			   
+			   
+			   
+			   
+			   
+			   
+			   
+	
+	
+	
+	
+	
+	
+	
